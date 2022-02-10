09949beed237a688073aa2b2c5bd34a109bea931
DoomyDe <abdoa0010@gmail.com>
2022-02-10 06:33:39 +0300

    Centered Logo, added hover  effect on nav links

--- WebServer_Office/views.py
+++ WebServer_Office/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def main_page(request):
-    return render(request, template_name="main_page.html")
+    return render(request, template_name="main_test.html")
 
 
 def main_page_ar(request):
